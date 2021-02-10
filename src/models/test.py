6ff57cabac80211ba427eaf19f83e691e3d995d9
Pauline <pauline.maury@visium.ch>
2021-02-10 16:47:03 +0000

    remove early break

--- src/models/test.py
+++ src/models/test.py
@@ -45,9 +45,6 @@ def get_test_data(data_loader, model, args):
             acc_embeddings = np.concatenate((acc_embeddings, out_features.cpu().data.numpy()), axis=0)
             acc_class = np.concatenate((acc_class, target.cpu().data.numpy()), axis=0)
 
-        if i > 1:
-            break
-
     return acc_fnames, acc_embeddings, acc_class
 
 
