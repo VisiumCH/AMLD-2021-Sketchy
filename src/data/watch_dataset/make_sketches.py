d5ee0f3a670d08e72c7cc410d4ddf08c6b6b8556
Pauline <pauline.maury@visium.ch>
2021-03-11 08:26:30 +0000

    remove empty files

--- src/data/watch_dataset/make_sketches.py
+++ src/data/watch_dataset/make_sketches.py
@@ -1,6 +1,7 @@
 import os
 from glob import glob
 import cv2 as cv
+import numpy as np
 
 from src.data.watch_dataset.hed import HED
 
@@ -9,30 +10,31 @@ from src.data.watch_dataset.hed import HED
 HED_MODEL_PATH = 'io/models/watch_pretrained_models/hed'
 
 
-def make_sketches(args):
+def make_sketches():
 
     hed = HED(HED_MODEL_PATH)
 
-    watches_folder = os.path.join(args.data_path, 'Watch')
+    watches_folder = 'io/data/raw/Watch'
 
     watches_images_path = glob(watches_folder + '/*/*/*/*image.png')
     number_images = len(watches_images_path)
 
     for i, image_path in enumerate(watches_images_path):
-        # Predict sketch from image
-        img = cv.imread(image_path)
-        pred = hed.predict(img)
-        cv.imwrite(image_path.replace('image.png', 'sketch.png'), 255*pred)
+        if i > 11388:
+            if i % 10 == 0:
+                print(f'{i} on {number_images}: {image_path}')
 
-        if i % 100 == 0:
-            print(f'{i} on {number_images}')
+            # Predict sketch from image
+            img = cv.imread(image_path)
+
+            if isinstance(img, np.ndarray):
+                pred = hed.predict(img)
+                cv.imwrite(image_path.replace('image.png', 'sketch.png'), 255*pred)
+            else:
+                print(f'\nImg {image_path} is not a numpy array.')
+                os.remove(image_path)
 
 
 if __name__ == '__main__':
-    from src.options import Options
 
-    # Parse options
-    args = Options().parse()
-    print("Parameters:\t" + str(args))
-
-    make_sketches(args)
+    make_sketches()
