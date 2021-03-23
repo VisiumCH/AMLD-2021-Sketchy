1d5455ae844553fcc755a1e329f39fb014b8b962
Pauline <pauline.maury@visium.ch>
2021-03-23 17:01:15 +0000

    better formatting without prettier linter

--- web_app/sketchy/src/App.js
+++ web_app/sketchy/src/App.js
@@ -1,11 +1,9 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom';
-import Drawing from './drawing.js'
-import Embeddings from './embeddingsPlot.js'
-
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Drawing from "./drawing.js";
+import Embeddings from "./embeddingsPlot.js";
 
 const App = () => {
-
   return (
     <Switch>
       <Route path="/drawing">
@@ -15,8 +13,7 @@ const App = () => {
         <Embeddings />
       </Route>
     </Switch>
-  )
-}
+  );
+};
 
-
-export default App
+export default App;
